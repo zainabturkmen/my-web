170712012c85ee0b3bacd1e449e999269ed77b58
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-07-02 08:26:04 +0430

    adding some design

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <section class="about-me" id="about">
+      <h2 class="section__title section__title--about">Who I am</h2>
+      <p class="section__subtitle section__subtitle--about">
+        Designer & developer based out of NYC
+      </p>
+
+      <div class="about-me__body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+
+      {/* <img src="img/dev-jane-02.jpg" alt="Jane leaning against a bus"> */}
+    </section>
   );
-}
+};
 
 export default About;
