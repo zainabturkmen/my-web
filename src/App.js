110b32d3a33aba100b3c5fe0a11fa4e538b47fb6
zainabturkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-07-02 18:03:44 +0430

    adding some design

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Services from "./components/Services";
 import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Home />
       <Services />
       <About />
+      <Projects />
     </div>
   );
 }
