63bfe22f4493f0b8b43b877e4a6a4d72eb33eed5
zainabturkmen <zainabturkmen7@gmail.com>
2024-07-14 15:31:58 +0430

    Adding some changes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 import Services from "./components/Services";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import { ContactUs } from "./components/ContactUs";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Services />
       <About />
       <Projects />
+      <ContactUs />
     </div>
   );
 }
